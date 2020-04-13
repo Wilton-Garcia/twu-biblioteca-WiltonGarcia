fc6c7dd2cdac75b22ac75ae887ba98bcfa1f9b1d
Wilton Garcia <wiltongarciaweb@gmail.com>
2020-04-13 01:46:23 -0300

    Book Class was add two atributes, author and year

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -2,16 +2,24 @@ package com.twu.biblioteca;
 
 public class Book {
     private String Name;
+    private String Author;
+    private int PublicationYear;
 
-    public Book(String name) {
-        Name = name;
+    public String getAuthor() {
+        return Author;
     }
 
+    public void setAuthor(String author) {
+        Author = author;
+    }
     public String getName() {
         return Name;
     }
 
     public void setName(String name) {
+        Name = name;
+    }
+    public Book(String name,String author, int publicationYear) {
         Name = name;
     }
 }
