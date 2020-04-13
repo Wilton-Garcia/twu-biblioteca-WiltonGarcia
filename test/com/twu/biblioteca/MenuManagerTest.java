ee7e4e22e9843e78bf2382a75f901c6f5a2d5a8f
Wilton Garcia <wiltongarciaweb@gmail.com>
2020-04-13 13:12:07 -0300

    Refefctoring test in MenuManagerTest

--- test/com/twu/biblioteca/MenuManagerTest.java
+++ test/com/twu/biblioteca/MenuManagerTest.java
@@ -8,17 +8,24 @@ public class MenuManagerTest {
 
 
     @Test
-    public void IsAValidaOptionShouldBeTrueTest() {
+    public void optionsIsValidWhenOptionIsAnIntegerNumber() {
         MenuManager menu = new MenuManager();
         assertEquals(menu.IsAValidOption("1"), true);
     }
 
     @Test
-    public  void IsAValidOptionShouldBeFalseTest(){
+    public void optionAreNotValidWhenOptionIsSpecialCharacter(){
         MenuManager menu = new MenuManager();
+        assertEquals(menu.IsAValidOption("/"), false);
+        assertEquals(menu.IsAValidOption("ç"), false);
+    }
+
+    @Test
+    public void optionAreNotValidWhenOptionsInEmptyOrNull(){
+        MenuManager menu = new MenuManager();
+        assertEquals(menu.IsAValidOption(null), false);
         assertEquals(menu.IsAValidOption(" "), false);
         assertEquals(menu.IsAValidOption(""), false);
-        assertEquals(menu.IsAValidOption("ç"), false);
     }
 
     @Test
